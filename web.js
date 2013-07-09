bde332d0db255bc10a25294a5e280f1ad6a739bc
Jinpei Li <jinpeiios@gmail.com>
2013-07-09 06:29:57 +0000

    update to read data from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(256);
+buf = fs.readFileSync('./index.html');
+var string = buf.toString('utf8',0,27);
+
+//console.log(string);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
